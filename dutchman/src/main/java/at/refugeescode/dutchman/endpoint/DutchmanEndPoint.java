a4a54eb08e7db933014e1df67b413bf4afd80d3c
nana80802 <miss_nisreen2000@yahoo.com>
2018-04-15 19:27:04 +0200

    Done with mp7

---
+++ dutchman/src/main/java/at/refugeescode/dutchman/endpoint/DutchmanEndPoint.java
@@ -0,0 +1,15 @@
+package at.refugeescode.dutchman.endpoint;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/deadmanschest")
+public class DutchmanEndPoint {
+
+    @GetMapping
+    String getChest() {
+        return "Haha Chest Is Here :D";
+    }
+}
